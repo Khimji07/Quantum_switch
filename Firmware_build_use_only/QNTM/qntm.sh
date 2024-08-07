b212f24f02d71d65ba0c6500cb473ae52aebbe3d
Khimji07 <kk.zengroup@gmail.com>
2024-08-07 12:11:13 +0530

    Update QN-Model

--- Firmware_build_use_only/QNTM/qntm.sh
+++ Firmware_build_use_only/QNTM/qntm.sh
@@ -8,7 +8,7 @@ if [ -d "$directory" ]; then
     echo "Service "QNTM_Serial" already Installed."
 else
     mkdir ../rootfs/common/etc/QNTM_Serial
-    cp -r QNTM_serial/* ../rootfs/common/etc/QNTM_Serial
+    cp -r QNTM_Serial/* ../rootfs/common/etc/QNTM_Serial
     
     echo "Service QNTM_Serial installed successfully"
 fi
